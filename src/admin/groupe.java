704cc3e5159011394b5cd6bba20a894fb238ff23
IDrissAitHafid <31975500+IDrissAitHafid@users.noreply.github.com>
2018-01-22 17:13:32 +0000

    creation classe groupe

--- src/admin/groupe.java
+++ src/admin/groupe.java
@@ -1,5 +1,33 @@
 package admin;
 
-public class groupe {
+import java.io.Serializable;
 
+public class groupe implements Serializable {
+	private int id_groupe;
+	private String Nom_groupe;
+	private String UE;
+	public groupe() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public groupe(String nom_groupe, String uE) {
+		super();
+		Nom_groupe = nom_groupe;
+		UE = uE;
+	}
+
+	public String getNom_groupe() {
+		return Nom_groupe;
+	}
+	public void setNom_groupe(String nom_groupe) {
+		Nom_groupe = nom_groupe;
+	}
+	public String getUE() {
+		return UE;
+	}
+	public void setUE(String uE) {
+		UE = uE;
+	}
+	
 }
